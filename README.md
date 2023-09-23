42fd68a079a990ab4acaf6650c958844fb7206dc
alliedeaver <adeav97@gmail.com>
2023-09-22 23:57:36 -0700

    added RAEDME

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # note-taker
-an application for taking notes 
+an application for taking notes and deleting them
+
+## Materials Used 
+VS code, node server, Heroku, were all materials used. The assistance of AskBCS was also implemented. 
+
+## Links 
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const routes = require('./routes');
 const path = require("path");
 const fs = require("fs");
 
-const data = require("./db.json");
+const data = require("./db/db.json");
 const PORT = 3002;
 
 const app = express();
@@ -14,15 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
-
 //GET/api/notes to read db.json
 
 app.get('/api/notes', (req, res) => res.json(data));
 
-//or 
-app.get('/', (req, res) =>
-res.send(path.join(__dirname, '/public/index.html'))
-);
 
 // POST/api/notes to read the db.json
 
@@ -35,35 +29,36 @@ app.post('/api/notes', (req, res) => {
     } else {
         res.status(400).json('Request body is missing');
     }
+
 });
 
 
 //POST/api/notes to receive a new note 
 app.post('/api/notes', (req, res) => {
     res.json(`${req.method} new note received`);
+
+    const { title, note } = req.body;
+    //give each note a unique ID
+    if (title && note) {
+        // Variable for the object we will save
+        const newNote = {
+            title,
+            note,
+            review_id: uuid(),
+        };
+
+    };
 })
 
-//give each note a unique ID
-
-if (title && note) {
-    // Variable for the object we will save
-    const newNote = {
-      title,
-      note,
-      review_id: uuid(),
-    };
-
-};
-
-    //GET for the notes.html
-    app.get('/', (req, res) =>
-        res.sendFile(path.join(__dirname, '/public/notes.html'))
-    );
+//GET for the notes.html
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes'))
+);
 
 
 //GET * to return index.html
-app.get* ('/api/', (req, res) => {
-    res.return(path,join(__dirname, '/public/index.html'))
+app.get('*')('/api/', (req, res) => {
+    res.return(path, join(__dirname, '/public/index.html'))
 })
 
 app.listen(PORT, () =>
